10725d6d6a4281b75e0c384becf0d7398f0ebe3d
Yangyang Zhao <yangyang.zhao@houzz.com>
2023-11-21 12:33:52 +0800

    Move folder

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Introduction",
+        "excerpt":"I started to get in touch with Istio from 2020, when the Istio version was still around 1.6-ish. I was not the person who introduced Istio to the company, there was another engineer who was also a newbie to Istio, spending some time studying it, then installed and created the...","categories": ["istio"],
+        "tags": [],
+        "url": "/blog/istio/2023/11/20/Introduction.html",
+        "teaser": null
+      },{
+        "title": "Istio Vs Envoy",
+        "excerpt":"If you looked at the documentation of Istio, this terminology envoy appears multiple ones. Also, there are some concepts like EnvoyFilter that you might come up in the Istio documentation. Assuming you’ve already had experience of instealling Istio, you may find there are two essential kubernetes components - istiod deployment...","categories": ["istio"],
+        "tags": [],
+        "url": "/blog/istio/2023/11/20/Istio-Vs-Envoy.html",
+        "teaser": null
+      },{
+        "title": "L4 Vs L7",
+        "excerpt":"As many of the students who have studied computer science, networking is definitely a part that is very important, but easy to forget after graduation. All the modern frameworks in different languages hide the details of networking from you, and you can use just 3 lines to make a HTTP...","categories": ["istio"],
+        "tags": [],
+        "url": "/blog/istio/2023/11/20/L4-Vs-L7.html",
+        "teaser": null
+      },{
+        "title": "Tuning Istio",
+        "excerpt":"Some of the new users of service mesh solutions including myself may think that network connections would be improved by just introducing the solution. This is wrong! In fact, ever since introducing Istio, we’ve met more way networking issues with workloads enabled service mesh, compared to the workloads that doesn’t....","categories": ["istio"],
+        "tags": [],
+        "url": "/blog/istio/2023/11/20/Tuning-Istio.html",
+        "teaser": null
+      }]
